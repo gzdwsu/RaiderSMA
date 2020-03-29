ccef9191f39f47ec956cb1c3f8559faf9d3a4b07
Steven Davis <59933104+sdavis00255@users.noreply.github.com>
2020-03-28 22:54:14 -0400

    Add files via upload

--- src/DataClasses/bowling/Bowler.java
+++ src/DataClasses/bowling/Bowler.java
@@ -16,8 +16,35 @@ public class Bowler extends Athlete{
 	
 	@Override
 	public String toString() {
-		return getFirstName() + " " + getLastName() + " #" + getNumber() + " \n\tRolls: " + scores + "\n";
+		return getFirstName() + " " + getLastName() + " #" + getNumber() + " Current Score: " + calculateScore() + " \n\tRolls: " + scores + "\n";
 	}
+	
+	public int calculateScore() {
+		
+        int score = 0;
+        int x = getRollsRolled();
+
+        if (x != 0) {
+            for (int i = 1; i != x+1; i++) {
+                score += getScore(i);
+                if (i > 4 && i < 20) { // Not Frames 1,2
+                    if (i % 2 != 0 ) { // 1st roll of frame 3-10
+                        if (getScore(i-2) == 10 || getScore(i-2) + getScore(i-1) == 10) score += getScore(i); // Previous frame was a mark
+                        if (getScore(i-4) == 10 && getScore(i-2) == 10) score += getScore(i); // Past 2 frames were a strike
+                    } else {
+                        if (getScore(i-3) == 10) score += getScore(i);  // 2nd roll of frame 3-10
+                    }
+                }
+                if (i == 20) {
+                    if (getScore(i-1) == 10) score += getScore(i);
+                }
+                if (i == 3) {
+                    if (getScore(i-2) + getScore(i-1) == 10) score += getScore(i);
+                }
+            }
+        }
+        return score;
+    }
 	
 	public void addScore(int score) {
 		scores.add(score);
